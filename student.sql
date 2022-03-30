8df5fe6e21b1987e54e07b565486c2f8374fd0b3
ian9598 <39250665+ian9598@users.noreply.github.com>
2022-03-30 16:59:07 +0800

    Update student.sql

--- student.sql
+++ student.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2022 at 08:23 AM
+-- Generation Time: Mar 30, 2022 at 08:50 AM
 -- Server version: 5.7.33
 -- PHP Version: 8.0.14
 
@@ -41,9 +41,8 @@ CREATE TABLE `student_info` (
 --
 
 INSERT INTO `student_info` (`student_id`, `student_name`, `gender`, `ic`, `created_at`, `updated_at`) VALUES
-('S00001', 'John Dae', 'Male', '810106-01-5119', '2022-03-29 20:15:54', '2022-03-29 23:22:07'),
-('S00002', 'Mike Ross', 'Male', '910106-01-3111', '2022-03-29 21:24:13', '2022-03-29 23:22:16'),
-('S00003', 'Rachel Zane', 'Female', '810106-01-5118', '2022-03-29 23:07:15', '2022-03-29 23:07:15');
+('S00001', 'John Dae', 'Male', '810106-01-5119', '2022-03-30 00:48:43', '2022-03-30 00:48:43'),
+('S00002', 'Rachel Zane', 'Female', '810106-01-5114', '2022-03-30 00:49:16', '2022-03-30 00:49:16');
 
 -- --------------------------------------------------------
 
@@ -54,7 +53,7 @@ INSERT INTO `student_info` (`student_id`, `student_name`, `gender`, `ic`, `creat
 CREATE TABLE `student_result` (
   `result_id` bigint(20) UNSIGNED NOT NULL,
   `course` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mark` float NOT NULL,
+  `mark` double(8,2) NOT NULL,
   `student_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -65,15 +64,10 @@ CREATE TABLE `student_result` (
 --
 
 INSERT INTO `student_result` (`result_id`, `course`, `mark`, `student_id`, `created_at`, `updated_at`) VALUES
-(5, 'CSC645', 87, 'S00001', '2022-03-29 23:05:32', '2022-03-29 23:24:05'),
-(10, 'CSC645', 89, 'S00003', '2022-03-29 23:19:25', '2022-03-29 23:19:25'),
-(11, 'CSC566', 41, 'S00003', '2022-03-29 23:19:38', '2022-03-29 23:19:38'),
-(12, 'CSC645', 100, 'S00002', '2022-03-29 23:19:48', '2022-03-29 23:23:08'),
-(13, 'CSC566', 89, 'S00002', '2022-03-29 23:19:55', '2022-03-29 23:19:55'),
-(14, 'CSC566', 67, 'S00001', '2022-03-29 23:22:38', '2022-03-29 23:22:38'),
-(15, 'CSC662', 34, 'S00002', '2022-03-29 23:22:52', '2022-03-29 23:22:52'),
-(16, 'CSC662', 81, 'S00003', '2022-03-29 23:23:24', '2022-03-29 23:23:30'),
-(17, 'CSC662', 77, 'S00001', '2022-03-29 23:23:53', '2022-03-29 23:23:53');
+(1, 'CSC645', 67.00, 'S00001', '2022-03-30 00:48:52', '2022-03-30 00:48:52'),
+(2, 'CSC662', 87.00, 'S00001', '2022-03-30 00:48:58', '2022-03-30 00:48:58'),
+(3, 'CSC645', 96.00, 'S00002', '2022-03-30 00:49:27', '2022-03-30 00:49:40'),
+(4, 'CSC662', 89.00, 'S00002', '2022-03-30 00:49:32', '2022-03-30 00:49:32');
 
 --
 -- Indexes for dumped tables
@@ -89,8 +83,7 @@ ALTER TABLE `student_info`
 -- Indexes for table `student_result`
 --
 ALTER TABLE `student_result`
-  ADD PRIMARY KEY (`result_id`),
-  ADD KEY `student_result_student_id_foreign` (`student_id`);
+  ADD PRIMARY KEY (`result_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -100,17 +93,7 @@ ALTER TABLE `student_result`
 -- AUTO_INCREMENT for table `student_result`
 --
 ALTER TABLE `student_result`
-  MODIFY `result_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `student_result`
---
-ALTER TABLE `student_result`
-  ADD CONSTRAINT `student_result_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `student_info` (`student_id`);
+  MODIFY `result_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
